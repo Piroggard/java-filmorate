4392a09f3f1e72e7ec486496fa4cd399a232693c
MaxF <piroggard@yandex.ru>
2023-05-26 18:14:42 +0300

    Project "Films" sprint 9 v1.7 Изменил версию SpringBoot

--- src/main/java/ru/yandex/practicum/filmorate/validation/ValidationFilm.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ValidationFilm.java
@@ -17,7 +17,6 @@ public class ValidationFilm {
         } else if (film.getDuration() <= 0) {
             throw new ValidationException("The film cannot be less than 1 minute long.");
         }
-
         LocalDate earlyReleaseDate = LocalDate.of(1895, 12, 28);
         if (film.getReleaseDate().isBefore(earlyReleaseDate)) {
             throw new ValidationException("The film cannot be earlier than December 28, 1895");
