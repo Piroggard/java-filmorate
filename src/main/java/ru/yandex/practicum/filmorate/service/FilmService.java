a62e4fcea1f25070c18144d0d5ad66911f309e0f
MaxF <piroggard@yandex.ru>
2023-06-14 00:10:16 +0300

    Добавление всего функционала по тз, Исправленный checkstyle FilmService V2

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,12 +4,20 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmService {
-   List<Film> getFilms();
-   Film postFilm(Film film);
+
+    List<Film> getFilms();
+
+    Film postFilm(Film film);
+
     Film putFilm(Film film);
+
     void addLikeFilm(int id, int userId);
+
     void deleteLikeFilm(int id, int userId);
+
     Film getFilm(Integer id);
+
     List<Film> getListBestMovies(Integer count);
+
     List<Film> getListBestTenMovies();
 }
