5b9b311dab7b1d0d338f86e6cf19fdcd52fa1516
MaxF <piroggard@yandex.ru>
2023-06-14 00:21:35 +0300

    Добавление всего функционала по тз, Исправленный checkstyle ValidationFilm V2

--- src/main/java/ru/yandex/practicum/filmorate/validation/ValidationFilm.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ValidationFilm.java
@@ -21,7 +21,7 @@ public class ValidationFilm {
     }
 
     public void validationIdFilm(int id) {
-        if (id < 1 ) {
+        if (id < 1) {
             throw new NullPointerException("ID cannot be negative");
         }
     }
