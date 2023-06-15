7358cb439df07f133c033aaa95429c777b034cfb
MaxF <piroggard@yandex.ru>
2023-06-15 22:03:37 +0300

    Добавление всего функционала по тз, Добавление нового исключения, Добавлен отдельный клас для обработки исключений поправил оформление кода

--- src/main/java/ru/yandex/practicum/filmorate/exception/DataNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/DataNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class DataNotFoundException extends NullPointerException{
+public class DataNotFoundException extends NullPointerException {
     public DataNotFoundException(String s) {
         super(s);
     }
