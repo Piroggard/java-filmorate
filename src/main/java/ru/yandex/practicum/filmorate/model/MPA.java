4a6c87c21478abc0cfb3ccc8a015a1edd4f5cac9
MaxF <piroggard@yandex.ru>
2023-09-02 17:25:33 +0300

    Готовое задание по тз исправил клас mpa

--- src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
@@ -7,7 +7,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class MPA implements Comparable<MPA>{
+
+public class MPA implements Comparable<MPA> {
     public MPA(Integer id) {
         this.id = id;
     }
