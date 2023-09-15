445f092f0b52a8809006e03d4843df7e167cf7df
MaxF <piroggard@yandex.ru>
2023-09-15 16:48:35 +0300

    Checkstyle V4

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.service;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genres;
@@ -42,7 +41,7 @@ public interface FilmService {
 
     Director putDirectors(Director director);
 
-    List<Film> getFilmDirectorYearOrLike (Integer directorId, List<String> sortBy);
+    List<Film> getFilmDirectorYearOrLike(Integer directorId, List<String> sortBy);
 
     void deleteDirectors(int id);
 }
