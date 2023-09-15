98159b400296b002c9419b0dbc8b0ddb49fca672
MaxF <piroggard@yandex.ru>
2023-09-15 16:59:54 +0300

    Checkstyle V7

--- src/main/java/ru/yandex/practicum/filmorate/validation/ValidationDirectors.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ValidationDirectors.java
@@ -4,8 +4,8 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Director;
 
 public class ValidationDirectors {
-    public void validation (Director director){
-        if (director.getName().length() == 0 || director.getName().equals(" ")){
+    public void validation(Director director) {
+        if (director.getName().length() == 0 || director.getName().equals(" ")) {
             throw new ValidationException("No director's name");
         }
     }
