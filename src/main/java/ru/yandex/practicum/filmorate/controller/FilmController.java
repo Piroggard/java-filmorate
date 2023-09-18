85948072e93591c6f28403598adfd9545e19e7cc
Dmitry <dmitri.marchenko96@yandex.ru>
2023-09-18 11:23:25 +0500

    checkstyle #1

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -52,6 +52,7 @@ public class FilmController {
     public List<Film> getListBestMovies(@RequestParam(required = false) Integer count) {
         return filmService.getListBestMovies(count);
     }
+
     @GetMapping("/films/common")
     public List<Film> getCommonFilms(@RequestParam int userId,
                                      @RequestParam int friendId) {
