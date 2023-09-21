fbcffd7ff913fcd7034e1a6f28ef9ec4e4774cb3
Dmitry <dmitri.marchenko96@yandex.ru>
2023-09-22 00:17:14 +0500

    check

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
@@ -225,10 +225,9 @@ public class UserDbStorage {
         }, keyHolder);
     }
 
-        public void deleteUser ( int userId){
+        public void deleteUser (int userId) {
             jdbcTemplate.update("delete from list_friends where id_friend = ?", userId);
             jdbcTemplate.update("delete from list_friends where id_user = ?", userId);
             jdbcTemplate.update("delete from users where id = ? ", userId);
-
         }
     }
